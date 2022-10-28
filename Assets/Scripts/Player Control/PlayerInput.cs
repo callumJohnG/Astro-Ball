c1c9b3147a65e48056957ee4b841da1aab4a0c9f
CallumG <gillespie.14.callum@gmail.com>
2022-10-28 21:14:38 +0100

    Added player character

--- Assets/Scripts/Player Control/PlayerInput.cs
+++ Assets/Scripts/Player Control/PlayerInput.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+
+    private CharacterController2D controller;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GatherInput();
+        ProcessInput();
+    }
+
+
+    private bool leftPressed;
+    [SerializeField] private KeyCode leftKey = KeyCode.W;
+    
+    private bool rightPressed;
+    [SerializeField] private KeyCode rightKey = KeyCode.D;
+    
+    private bool jumpPressed;
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+
+    private float horizontalMovement;
+
+    private void GatherInput(){
+        horizontalMovement = Input.GetAxis("Horizontal");
+
+        leftPressed = Input.GetKey(leftKey);
+        rightPressed = Input.GetKey(rightKey);
+        jumpPressed = Input.GetKeyDown(jumpKey);
+    }
+
+    private void ProcessInput(){
+        controller.Move(horizontalMovement, false, jumpPressed);
     }
 }
