8199d734e823e48f57a6269df2fa7e03184e7b0f
Callum Gillespie <gillespie.14.callum@gmail.com>
2022-12-20 15:22:49 -0300

    Added point gain when ascending

---
+++ Assets/Scripts/Object Scripts/Player/HeightPoints.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightPoints : MonoBehaviour
+{
+
+    [SerializeField] private int pointsPerUnit;
+    [SerializeField] private float unitHeight;
+
+    private float maxHeight;
+    private float heightRemainder = 0;
+
+    private void Start() {
+        maxHeight = transform.position.y;
+    }
+
+    void Update()
+    {
+        CheckHeight();
+    }
+
+    private void CheckHeight(){
+        float currentY = transform.position.y;
+        if(currentY <= maxHeight){
+            return;
+        }
+
+        //Get the difference between past height and currentHeight
+        float heightDifference = currentY - maxHeight;
+        maxHeight = currentY;
+
+        //Add on the remainder from previous frame
+        heightDifference += heightRemainder;
+
+        //Calculate how many segments we have crossed with this height difference
+        int unitsPassed = Mathf.FloorToInt(heightDifference / unitHeight);
+
+        //Add points equal to units passed
+        PointsManager.Instance.GainPoints(unitsPassed * pointsPerUnit);
+
+        //Set the next remainder for next frame
+        heightRemainder = heightDifference % unitHeight;
+
+
+    }
+}
