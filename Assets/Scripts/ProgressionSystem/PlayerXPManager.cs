17582fda9b342672670b726268b45d8646d99674
Callum Gillespie <gillespie.14.callum@gmail.com>
2022-12-23 11:33:40 -0300

    Added logarithmic point gain

--- Assets/Scripts/ProgressionSystem/PlayerXPManager.cs
+++ Assets/Scripts/ProgressionSystem/PlayerXPManager.cs
@@ -13,6 +13,11 @@ public class PlayerXPManager : MonoBehaviour
         InitialiseXP();
     }
 
+    private void Start() {
+        CreateSegments();
+        //TestSegments();
+    }
+
     [SerializeField] private float pointsPerXP = 150; //The amount of points needed to gain 1 xp
     private const string XP_KEY = "xp";
 
@@ -21,6 +26,7 @@ public class PlayerXPManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI gainedXPText;
 
     [SerializeField] private GainXPAnimation xpAnimation;
+    [SerializeField] private List<PointSegment> segments;
 
     private void InitialiseXP(){
         int currentXP = PlayerPrefs.GetInt(XP_KEY, 0);
@@ -55,20 +61,75 @@ public class PlayerXPManager : MonoBehaviour
     }
 
     public void CalculateXPGain(int points){
-        Debug.Log("Gaining xp from " + points + " points");
+        /*Debug.Log("Gaining xp from " + points + " points");
         int newXP = Mathf.FloorToInt(points/pointsPerXP);
 
         xpAnimation.SetUpValues(newXP, GetXP());
         xpAnimation.PlayXPAnimation();
 
+        GainXP(newXP);*/
+
+        int newXP = CalculateXPGainFromSegments(points);
         GainXP(newXP);
 
         //gainedXPText.text = "Gained +" + newXP + " coins";
+    }
+
+    private void CreateSegments(){
+        segments = new List<PointSegment>();
+        segments.Add(new PointSegment(0, 500, 30));
+        segments.Add(new PointSegment(501, 1000, 70));
+        segments.Add(new PointSegment(1001, 2500, 100));
+        segments.Add(new PointSegment(2501, 5000, 180));
+        segments.Add(new PointSegment(5001, 10000, 400));
+        segments.Add(new PointSegment(10001, Mathf.Infinity, 800));
+    }
+
+    public int CalculateXPGainFromSegments(int points){
+        int remainder = points;
+        int result = 0;
+        foreach(PointSegment seg in segments){
+            if(points < seg.minRange) return result;
+
+            if(points < seg.maxRange){
+                //We end on this loop
+                //Get the number of points inside this segment
+                int pointsInSegment = points - (int)seg.minRange;
+                int segmentResult = Mathf.FloorToInt(pointsInSegment / seg.pointsPerXP);
+                result += segmentResult;
+                return result;
+            } else {
+                int pointsInSegment = (int)seg.maxRange - (int)seg.minRange;
+                int segmentResult = Mathf.FloorToInt(pointsInSegment / seg.pointsPerXP);
+                result += segmentResult;
+            }
+        }
+
+        return result;
+    }
+
+    private void TestSegments(){
+        List<int> testPoints = new List<int>{0, 50, 100, 200, 350, 414, 689, 899, 1049, 2005, 5310, 20240};
+        foreach(int point in testPoints){
+            Debug.Log(point + ":" + CalculateXPGainFromSegments(point));
+        }
     }
 
     private void UpdateTexts(){
         foreach(TextMeshProUGUI display in playerXPTexts){
             display.text = GetXP().ToString();
         }
+    }
+}
+
+public struct PointSegment{
+    public float minRange;
+    public float maxRange;
+    public int pointsPerXP; 
+
+    public PointSegment(float minRange, float maxRange, int pointsPerXP){
+        this.minRange = minRange;
+        this.maxRange = maxRange;
+        this.pointsPerXP = pointsPerXP;
     }
 }
