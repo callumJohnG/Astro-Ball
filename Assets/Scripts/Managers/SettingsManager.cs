4fa244273222a21f79ed0485073b1f92ea7e3a5c
CallumG <gillespie.14.callum@gmail.com>
2023-04-12 13:56:02 +0100

    Memory on settings sliders. Pixelation slider large increments

--- Assets/Scripts/Managers/SettingsManager.cs
+++ Assets/Scripts/Managers/SettingsManager.cs
@@ -13,48 +13,101 @@ public class SettingsManager : MonoBehaviour
 
     private void Start() {
         SetUpAimSetting();
+        GetPixelation();
+        GetMusic();
+        GetSFX();
     }
 
 
     [Header("Music")]
     [SerializeField] private float maxMusicVolume;
     [SerializeField] private float minMusicVolume;
+    [SerializeField] private Slider musicSlider;
+    private const string MUSIC_KEY = "Music_Key";
 
     public void SetMusicVolume(float value){
-        float increment = (Mathf.Abs(maxMusicVolume - minMusicVolume)) / 25;
+        //float increment = (Mathf.Abs(maxMusicVolume - minMusicVolume)) / 25;
+        //float newValue = minMusicVolume + (value * increment);
+        //if(value == 0) newValue = -80;
 
-        float newValue = minMusicVolume + (value * increment);
-
-        if(value == 0) newValue = -80;
+        PlayerPrefs.SetFloat(MUSIC_KEY, value);
         
-        audioMixer.SetFloat("MusicVolume", newValue);
+        //audioMixer.SetFloat("MusicVolume", newValue);
+    }
+    private void GetMusic(){
+        float value = PlayerPrefs.GetFloat(MUSIC_KEY, maxMusicVolume);
+
+        musicSlider.minValue = minMusicVolume;
+        musicSlider.maxValue = maxMusicVolume;
+
+        musicSlider.value = value;
+        //SetMusicVolume(value);
     }
 
     [Header("SFX")]
     [SerializeField] private float maxSFXVolume;
     [SerializeField] private float minSFXVolume;
+    [SerializeField] private Slider SFXSlider;
+    private const string SFX_KEY = "SFX_Key";
 
     public void SetSFXVolume(float value){
-        float increment = (Mathf.Abs(maxSFXVolume - minSFXVolume)) / 20;
-
-        float newValue = minSFXVolume + (value * increment);
-
-        if(value == 0) newValue = -80;
+        //float increment = (Mathf.Abs(maxSFXVolume - minSFXVolume)) / 20;
+        //float newValue = minSFXVolume + (value * increment);
+        //if(value == 0) newValue = -80;
         
-        audioMixer.SetFloat("SFXVolume", newValue);
+        PlayerPrefs.SetFloat(SFX_KEY, value);
+        
+        //audioMixer.SetFloat("SFXVolume", newValue);
+    }
+    private void GetSFX(){
+        float value = PlayerPrefs.GetFloat(SFX_KEY, maxSFXVolume);
+
+        SFXSlider.minValue = minSFXVolume;
+        SFXSlider.maxValue = maxSFXVolume;
+
+        SFXSlider.value = value;
+        //SetSFXVolume(value);
     }
 
-    [Header("Pixilation")]
-    [SerializeField] private float maxPixilation;
-    [SerializeField] private float minPixilation;
+    [Header("Pixelation")]
+    [SerializeField] private float smallPixelSize;
+    [SerializeField] private float midPixelSize;
+    [SerializeField] private float bigPixelSize;
+    [SerializeField] private Slider pixelSlider;
+    private const string PIXELATION_KEY = "Pixelation_Key";
+    [SerializeField] private int defaultPixelValue = 1;
 
-    public void SetPixelation(float value){
-        float increment = (Mathf.Abs(maxPixilation - minPixilation)) / 25;
+    public void SetPixelation(float valuef){
+        int value = Mathf.RoundToInt(valuef);
+        switch (value){
+            case 0:
+                //Turn off pixelation
+                shaderMaterialManager.SetPixelation(0f);
+                break;
+            case 1:
+                //Turn off pixelation
+                shaderMaterialManager.SetPixelation(smallPixelSize);
+                break;
+            case 2:
+                //Turn off pixelation
+                shaderMaterialManager.SetPixelation(midPixelSize);
+                break;
+            case 3:
+                //Turn off pixelation
+                shaderMaterialManager.SetPixelation(bigPixelSize);
+                break;
+        }
 
-        float newValue = minPixilation + (value * increment);
+        PlayerPrefs.SetInt(PIXELATION_KEY, value);
 
-        shaderMaterialManager.SetPixelation(newValue);
     }
+
+    private void GetPixelation(){
+        int value = PlayerPrefs.GetInt(PIXELATION_KEY, defaultPixelValue);
+        pixelSlider.value = value;
+        SetPixelation(value);
+    }
+
 
     [Header("Controls")]
     [SerializeField] private bool inverseAiming = false;
